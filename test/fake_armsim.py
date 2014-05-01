8e0f4b38723fffdeb34120cc7c25ff5514124207
Sergio Barrachina Mir <barrachi@uji.es>
2014-05-01 13:25:52 +0200

    Primera versión del test de comunicación completada

--- test/fake_armsim.py
+++ test/fake_armsim.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Fake armsim. Used to try socket communication between qarmsim and
-# armsim while defining its grammar.
+# Fake armsim. Used to try socket communication between Qt ARMSim and ARMSim
+# while defining the communication grammar.
 
 import sys
 import getopt
-import string
-from src.mysocket import MySocket
+from mysocket import MySocket
 
+# Globals
 PORT = 0
+mysocket = None
 
 def myhelp():
     print("""Usage: fake_armsim.py -p PORTNUMBER
 
-This is a fake armsim with socket server capabilities. Please, use the
--p option to indicate which port number should be used.
+This is a fake ARMSim with socket server capabilities. Please, use the -p option
+to indicate which port number should be used.
 
 Options:
    -v, --verbose     increment the output verbosity
@@ -32,8 +33,8 @@ def ERROR(text):
 def getopts():
     "Processes the options passed to the executable"
     global PORT
-    optlist, args = getopt.getopt(sys.argv[1:], \
-                                  'p:h', \
+    optlist, args = getopt.getopt(sys.argv[1:],         # @UnusedVariable
+                                  'p:h',
                                   ['port:', 'help', ])
     for opt, arg in optlist:
         if opt in ('-h', '--help'):
@@ -45,7 +46,9 @@ def getopts():
         myhelp()
         ERROR("a port number must be specified.")
 
-
+# Fake registers
+reg_names = ['r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7',
+             'PC', 'SP', 'LR', 'CPSR']
 _REGISTERS = {
     'r0': "0x00000000",
     'r1': "0x00000000",
@@ -62,71 +65,170 @@ _REGISTERS = {
     }
 REGISTERS = _REGISTERS.copy()
 
+# Fake memory
 _MEMORY = ["0x00"]*10000
 MEMORY = _MEMORY[:]
 
+# Fake breakpoints
+BREAKPOINTS = []
+
+# Literals
 OK = 'OK'
+EOF = 'EOF'
+
+
+#===============================================================================
+# SHOW commands
+#===============================================================================
 
 def do_SHOW_VERSION(args, socket):
     print("Showing version information")
-    socket.send_line("fake_armsim version 0.1")
+    socket.send_line("Fake ARMSim version 0.1")
     socket.send_line("(c) 2014 Sergio Barrachina Mir")
-    socket.send_line("EOF")
+    socket.send_line(EOF)
 
 def do_SHOW_REGISTER(args, socket):
-    reg = args[2]
-    print("Showing contents of register {}".format(reg))
-    socket.send_line(REGISTERS[reg])
+    reg_name = args[2]
+    print("Showing contents of register {} ({})".format(reg_name, REGISTERS[reg_name]))
+    socket.send_line("{}: {}".format(reg_name, REGISTERS[reg_name]))
+
+def do_SHOW_MEMORY(args, socket):
+    "SHOW MEMORY (BYTE|HALF|WORD) AT ADDRESS"
+    size = args[2]
+    hex_address = args[4]
+    print("Showing {} at Memory[{}]".format(size.lower(), hex_address))
+    address = int(hex_address, 16)
+    if size == 'BYTE':
+        socket.send_line("{}: {}".format(hex_address, MEMORY[address]))
+    elif size == 'HALF':
+        # Little-endian version
+        value = MEMORY[address] + MEMORY[address+1][2:]
+        socket.send_line("{}: {}".format(hex_address, value))
+    elif size == 'WORD':
+        # Little-endian version
+        value = MEMORY[address] + MEMORY[address+1][2:] + MEMORY[address+2][2:] + MEMORY[address+3][2:]
+        socket.send_line("{}: {}".format(hex_address, value))
     
 def do_DUMP_REGISTERS(args, socket):
     print("Dumping all the registers")
-    for reg in range(len(REGISTERS)):
-        socket.send_line(REGISTERS[reg])
+    for reg_name in reg_names:
+        socket.send_line("{}: {}".format(reg_name, REGISTERS[reg_name]))
 
 def do_DUMP_MEMORY(args, socket):
-    start = int(args[2])
-    nbytes = int(args[3])    
+    "DUMP MEMORY START_ADDRESS NBYTES"
+    start = int(args[2], 16)
+    nbytes = int(args[3])  
     print("Dumping memory starting at address {} ({} bytes)".format(start, nbytes))
-    for pos in range(start, start+nbytes+1):
-        socket.send_line(MEMORY[pos])
+    for pos in range(start, start+nbytes):
+        socket.send_line("{}: {}".format("0x{0:0{1}X}".format(pos, 8), MEMORY[pos]))
+
+def do_SHOW_BREAKPOINTS(args, socket):
+    "SHOW BREAKPOINTS"
+    print("Showing breakpoints")
+    for breakpoint in BREAKPOINTS:
+        socket.send_line(breakpoint)
+    socket.send_line(EOF)
+
+
+#===============================================================================
+# SET commands
+#===============================================================================
 
 def do_SET_REGISTER(args, socket):
-    reg = int(args[2])
-    value = args[3]
-    print("Setting register {} to {}".format(reg, value))
-    REGISTERS[reg] = value
+    "SET REGISTER regname WITH hexvalue"
+    reg_name = args[2]
+    value = args[4]
+    print("Setting register {} to {}".format(reg_name, value))
+    REGISTERS[reg_name] = value
+    socket.send_line(OK)
 
 def do_SET_MEMORY(args, socket):
-    pos = int(args[2])
-    value = args[3]
-    print("Setting memory[{}] to {}".format(pos, value))
-    MEMORY[pos] = value
+    "SET MEMORY (BYTE|HALF|WORD) AT address WITH hexvalue"
+    size = args[2]
+    hex_pos = args[4]
+    pos = int(hex_pos, 16)
+    value = args[6]
+    print("Setting {} at memory[{}] to {}".format(size.lower(), hex_pos, value))
+    if size == 'BYTE':
+        MEMORY[pos] = value
+    elif size == 'HALF':
+        MEMORY[pos] = value[:4]
+        MEMORY[pos+1] = "0x{}".format(value[4:])
+    elif size == 'WORD':
+        MEMORY[pos] = value[:4]
+        MEMORY[pos+1] = "0x{}".format(value[4:6])
+        MEMORY[pos+2] = "0x{}".format(value[6:8])
+        MEMORY[pos+3] = "0x{}".format(value[8:])
+    socket.send_line(OK)
+        
+def do_SET_BREAKPOINT_AT(args, socket):
+    "SET BREAKPOINT AT address"
+    breakpoint = args[3]
+    print("Setting breakpoint at {}".format(breakpoint))
+    BREAKPOINTS.append(breakpoint)
+    socket.send_line(OK)
+
+#===============================================================================
+# RESET commands
+#===============================================================================
 
 def do_RESET_REGISTERS(args, socket):
+    global REGISTERS
+    print("Reseting registers")
     REGISTERS = _REGISTERS.copy()
     socket.send_line(OK)
 
 def do_RESET_MEMORY(args, socket):
+    global MEMORY
+    print("Reseting memory")
     MEMORY = _MEMORY[:]
     socket.send_line(OK)
+
+#===============================================================================
+# CLEAR commands
+#===============================================================================
+
+def do_CLEAR_BREAKPOINTS(args, socket):
+    "CLEAR BREAKPOINTS"
+    global BREAKPOINTS
+    print("Clearing breakpoints")
+    BREAKPOINTS = []
+    socket.send_line(OK)
+    
+def do_CLEAR_BREAKPOINT_AT(args, socket):
+    "CLEAR BREAKPOINT AT"
+    breakpoint = args[3]
+    print("Clearing breakpoint at {}".format(breakpoint))
+    BREAKPOINTS.remove(breakpoint)
+    socket.send_line(OK)
+
+#===============================================================================
+# def signal_handler(signal, frame):
+#     if mysocket != None:
+#         mysocket.close_socket()
+#     sys.exit(0)
+#===============================================================================
 
 def main():
     "Main part of the application"
     getopts()
-    mysocket = MySocket()
+    #signal.signal(signal.SIGINT, signal_handler)
+    mysocket = MySocket(verbose = True)
     mysocket.server_bind(PORT)
-    mysocket.server_accept_connection()
-    lines_generator = mysocket.get_lines()
-    for line in lines_generator:
-        args = line.split(' ')
-        for n in range(len(args), 0, -1):
-            do_method_name = 'do_{}'.format('_'.join(args[0:n]))
-            do_method = globals().get(do_method_name)
-            if do_method:
-                do_method(args, mysocket)
-                break
-        else:
-            print(">>> Unsupported command: {}".format(line))
+    while True:
+        print("")
+        mysocket.server_accept_connection()
+        lines_generator = mysocket.get_lines()
+        for line in lines_generator:
+            args = line.split(' ')
+            for n in range(len(args), 0, -1):
+                do_method_name = 'do_{}'.format('_'.join(args[0:n]))
+                do_method = globals().get(do_method_name)
+                if do_method:
+                    do_method(args, mysocket)
+                    break
+            else:
+                print(">>> Unsupported command: {}".format(line))
     mysocket.close_connection()
     mysocket.close_socket()
 
--- test/test_armsim.py
+++ test/test_armsim.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Fake armsim. Used to try socket communication between qarmsim and
-# armsim while defining its grammar.
+# Fake ARMSim. Used to try socket communication between Qt ARMSim and
+# ARMSim while defining its grammar.
 
 import sys
 import getopt
-import string
-from src.mysocket import MySocket
+from mysocket import MySocket
 import unittest
+import socket
 
 PORT = 8010
 
 def myhelp():
     print("""Usage: test_armsim.py
 
-This application is used to test the communication between qarmsim and
+This application is used to test the communication between Qt ARMSim and
 the simulator. Before running this test, the simulator has to be
 already running and listening in the port {}.
 
 Options:
-   -v, --verbose     increment the output verbosity
    -h, --help        display this help and exit
 
 Please, report bugs to <barrachi@uji.es>.
@@ -33,79 +32,266 @@ def ERROR(text):
 
 def getopts():
     "Processes the options passed to the executable"
-    optlist, args = getopt.getopt(sys.argv[1:], \
-                                  'h', \
+    optlist, args = getopt.getopt(sys.argv[1:], # @UnusedVariable
+                                  'h',
                                   ['help', ])
-    for opt, arg in optlist:
+    for opt, arg in optlist:  # @UnusedVariable
         if opt in ('-h', '--help'):
             myhelp()
             sys.exit()
 
 
-mysocket = MySocket()
 OK = 'OK'
+EOF = 'EOF'
 reg_names = ['r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7',
              'PC', 'SP', 'LR', 'CPSR']
 
-class TestCommunication(unittest.TestCase):
+class CommunicationTestCase(unittest.TestCase):
+
+    #===========================================================================
+    # Pre and post test actions         
+    #===========================================================================
 
     def setUp(self):
-        "This gets executed before each test"
-        mysocket.send_line("RESET REGISTERS")
-        mysocket.send_line("RESET MEMORY")
-        pass
+        "This is executed before each one of the tests"
+        self.mysocket = MySocket()
+        try:
+            self.mysocket.connect_to(PORT)
+        except ConnectionRefusedError:
+            ERROR("Couldn't connect to the simulator. Please, check that the simulator\n"
+                  "       is listening on port {}.".format(PORT))
+        # Usual operation will be blocking(?), but here we put the timeout to 2 seconds
+        self.mysocket.sock.settimeout(2.0) # Set timeout to 2 seconds
+        self.mysocket.send_line("RESET REGISTERS")
+        if (self.mysocket.receive_line() != OK):
+            ERROR('RESET REGISTERS should return an OK')
+        self.mysocket.send_line("RESET MEMORY")
+        if (self.mysocket.receive_line() != OK):
+            ERROR('RESET MEMORY should return an OK')
+
+    def tearDown(self):
+        "This gets executed after each test"
+        self.mysocket.close_connection()
+        self.mysocket.close_socket()
+
+    
+    #===========================================================================
+    # Show version test         
+    #===========================================================================
+
+    def test_show_version(self):
+        "SHOW VERSION should return one or more lines ended by an EOF line."
+        self.mysocket.send_line("SHOW VERSION")
+        line = ''
+        while line != 'EOF':
+            line = self.mysocket.receive_line()
+        self.assertEqual(line, EOF)
+
+    
+    #===========================================================================
+    # Register tests         
+    #===========================================================================
+    
+    def test_show_register_r0(self):
+        "SHOW REGISTER r0 should return 'r0: 0x00000000' after RESET REGISTERS"
+        self.mysocket.send_line("SHOW REGISTER r0")
+        self.assertEqual(self.mysocket.receive_line(), "r0: 0x00000000")
+
+    def test_set_register_r1(self):
+        "SET REGISTER 1 WITH 0x00000005 should overwrite register r1 with 0x00000005 and return OK."
+        self.mysocket.send_line("SET REGISTER r1 WITH 0x00000005")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW REGISTER r1")
+        self.assertEqual(self.mysocket.receive_line(), "r1: 0x00000005")
 
     def test_reset_registers(self):
         "RESET REGISTERS should reset all registers to their initial value."
         for reg_name in reg_names:
-            mysocket.send_line("SET REGISTER {} 0x12345678".format(reg_name)) 
-        mysocket.send_line("RESET REGISTERS")
-        self.assertEqual(mysocket.receive_line(), OK)
+            self.mysocket.send_line("SET REGISTER {} WITH 0x12345678".format(reg_name)) 
+            self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("RESET REGISTERS")
+        self.assertEqual(self.mysocket.receive_line(), OK)
         # Check that r0 is reset to 0x00000000
-        mysocket.send_line("SHOW REGISTER {}".format('r0'))
-        self.assertEqual(mysocket.receive_line(), "r0: 0x00000000")
-        # Check that all the registers have a value different of 0x12345678
-        for reg_name in reg_names:
-            mysocket.send_line("SHOW REGISTER {}".format(reg_name))
-            answer = mysocket.receive_line()
-            (reg, value) = answer.split(": ")
-            self.assertNotEqual(value, "0x00000000")
-        
-
+        self.mysocket.send_line("SHOW REGISTER r0")
+        self.assertEqual(self.mysocket.receive_line(), "r0: 0x00000000")
+        # Check that the rest of the registers have a value different of 0x12345678
+        for reg_name in reg_names[1:]:
+            self.mysocket.send_line("SHOW REGISTER {}".format(reg_name))
+            answer = self.mysocket.receive_line()
+            value = answer.split(": ")[1]
+            self.assertNotEqual(value, "0x12345678")
+         
     def test_dump_registers(self):
         "DUMP REGISTERS should return the value of all the registers."
-        
-    def test_set_register_1(self):
-        "SET REGISTER 1 0x00000005 should overwrite register R1 with 0x00000005."
-        mysocket.send_line("SET REGISTER 1 0x00000005")
-        mysocket.send_line("SHOW REGISTER 1")
-        line = mysocket.receive_line()
-        self.assertEqual(line, "0x00000004")
+        self.mysocket.send_line("DUMP REGISTERS")
+        for reg_name in reg_names:
+            line = self.mysocket.receive_line()
+            part_tested = "{}: 0x".format(reg_name)
+            self.assertEqual(line[:len(part_tested)], part_tested)
 
-    def test_show_register_r0(self):
-        "SHOW REGISTER r0 should return value 0x00000000 after RESET REGISTERS"
-        mysocket.send_line("SHOW REGISTER r0")
-        line = mysocket.receive_line()
-        self.assertEqual(line, "r0: 0x00000000")
+    #===========================================================================
+    # Memory tests         
+    #===========================================================================
+    
+    def test_show_memory_byte(self):
+        "SHOW MEMORY BYTE AT 0x00000020 should return '0x00000020: 0x00' after RESET MEMORY"
+        self.mysocket.send_line("SHOW MEMORY BYTE AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x00")
 
-    def test_show_version(self):
-        "SHOW VERSION should return one or more lines ended by an EOF line."
-        mysocket.send_line("SHOW VERSION")
-        mysocket.sock.settimeout(2.0) # Set timeout to 2 seconds
-        line = ''
-        while line != 'EOF':
-            line = mysocket.receive_line()
-        mysocket.sock.settimeout(None) # Set blocking mode again
-            
-    def tearDown(self):
-        "This gets executed after each test"
-        pass
+    def test_show_memory_half(self):
+        "SHOW MEMORY HALF AT 0x00000020 should return '0x00000020: 0x0000' after RESET MEMORY"
+        self.mysocket.send_line("SHOW MEMORY HALF AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x0000")
+
+    def test_show_memory_word(self):
+        "SHOW MEMORY WORD AT 0x00000020 should return '0x00000020: 0x00000000' after RESET MEMORY"
+        self.mysocket.send_line("SHOW MEMORY WORD AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x00000000")
+
+    def test_set_memory_byte(self):
+        "SET MEMORY BYTE AT 0x00000020 WITH 0X10 should overwrite that byte with 0x10 and return OK."
+        self.mysocket.send_line("SET MEMORY BYTE AT 0x00000020 WITH 0x10")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW MEMORY BYTE AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x10")
+
+    def test_set_memory_half(self):
+        "SET MEMORY HALF AT 0x00000020 WITH 0X1020 should overwrite that half with 0x1020 and return OK."
+        self.mysocket.send_line("SET MEMORY HALF AT 0x00000020 WITH 0x1020")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW MEMORY HALF AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x1020")
+
+    def test_set_memory_word(self):
+        "SET MEMORY WORD AT 0x00000020 WITH 0X10203040 should overwrite that word with 0x10203040 and return OK."
+        self.mysocket.send_line("SET MEMORY WORD AT 0x00000020 WITH 0x10203040")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW MEMORY WORD AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x10203040")
+
+    def test_endianess(self):
+        "Check that little-endian is being used for storing halfs and words."
+        self.mysocket.send_line("SET MEMORY BYTE AT 0x00000020 WITH 0x10")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SET MEMORY BYTE AT 0x00000021 WITH 0x20")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SET MEMORY BYTE AT 0x00000022 WITH 0x30")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SET MEMORY BYTE AT 0x00000023 WITH 0x40")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW MEMORY HALF AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x1020")
+        self.mysocket.send_line("SHOW MEMORY WORD AT 0x00000020")
+        self.assertEqual(self.mysocket.receive_line(), "0x00000020: 0x10203040")
+
+    def test_reset_memory(self):
+        "RESET MEMORY should reset all the memory to its initial value."
+        for pos in range(20):
+            hex_pos = "0x{0:0{1}X}".format(pos, 8)
+            self.mysocket.send_line("SET MEMORY BYTE AT {} WITH {}".format(hex_pos, "0xA0"))
+            self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("RESET MEMORY")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        # Check that the read bytes a value different of 0xA0
+        for pos in range(20):
+            hex_pos = "0x{0:0{1}X}".format(pos, 8)
+            self.mysocket.send_line("SHOW MEMORY BYTE AT {}".format(hex_pos))
+            answer = self.mysocket.receive_line()
+            value = answer.split(": ")[1]
+            self.assertNotEqual(value, "0xA0")
+
+    def test_dump_memory(self):
+        "DUMP MEMORY start_address nbytes should return the value of the nbytes from start_address."
+        self.mysocket.send_line("DUMP MEMORY 0x00000000 20")
+        for pos in range(20):
+            hex_pos = "0x{0:0{1}X}".format(pos, 8)
+            self.assertEqual(self.mysocket.receive_line(), "{}: 0x00".format(hex_pos))
+        self.assertRaises(socket.timeout, self.mysocket.receive_line)
+
+    #===========================================================================
+    # Breakpoint tests         
+    #===========================================================================
+    
+    def test_breakpoints(self):
+        "Test clear, set, and show breakpoints"
+        #--------------------------------------------------
+        # Method used to get the breakpoints from the simmulator
+        #--------------------------------------------------
+        def get_breakpoints():
+            line = ''
+            sim_breakpoints = []
+            self.mysocket.send_line("SHOW BREAKPOINTS")
+            while (line != EOF):
+                line = self.mysocket.receive_line()
+                if line != EOF:
+                    self.assertEqual(line[:2], "0x")
+                    sim_breakpoints.append(line)
+            return sim_breakpoints
+        #--------------------------------------------------
+        # "Clear breakpoints" test (firs pass)
+        #--------------------------------------------------
+        self.mysocket.send_line("CLEAR BREAKPOINTS")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW BREAKPOINTS")
+        self.assertEqual(self.mysocket.receive_line(), EOF, "Expecting EOF after SHOW BREAKPOINTS")
+        #--------------------------------------------------
+        # "Set breakpoint" at test        
+        #--------------------------------------------------
+        breakpoints = ["0x10102020", "0x20203030", "0x30304040"]
+        for breakpoint in breakpoints:
+            self.mysocket.send_line("SET BREAKPOINT AT {}".format(breakpoint))
+            self.assertEqual(self.mysocket.receive_line(), OK)
+        sim_breakpoints = get_breakpoints()
+        self.assertCountEqual(sim_breakpoints, breakpoints, "Stored breakpoints differ from setted ones")
+        #--------------------------------------------------
+        # "Clear breakpoint at" test
+        #--------------------------------------------------
+        self.mysocket.send_line("CLEAR BREAKPOINT AT 0x10102020")
+        self.assertEqual(self.mysocket.receive_line(), OK, "Expecting OK after CLEAR BREAKPOINT AT 0x10102020")
+        breakpoints.remove("0x10102020")
+        sim_breakpoints = get_breakpoints()
+        self.assertCountEqual(sim_breakpoints, breakpoints, "Stored breakpoints differ from setted ones after removing the one at address 0x101012020")
+        #--------------------------------------------------
+        # "Clear breakpoints" test (second pass)
+        #--------------------------------------------------
+        self.mysocket.send_line("CLEAR BREAKPOINTS")
+        self.assertEqual(self.mysocket.receive_line(), OK)
+        self.mysocket.send_line("SHOW BREAKPOINTS")
+        self.assertEqual(self.mysocket.receive_line(), EOF, "Expecting EOF after SHOW BREAKPOINTS")        
         
+        
+        
+    #===========================================================================
+    # Disassemble tests         
+    #===========================================================================
+    
+    #===========================================================================
+    # Execute tests         
+    #===========================================================================
+    
+    #===========================================================================
+    # Assemble tests         
+    #===========================================================================
+    
+
+def suite():
+    '''
+    Used to manually define the suite of tests that should be executed.
+    '''
+    # To check specific tests, change manual_suite to True and add as addTest
+    # lines are required.
+    manual_suite = False
+    if manual_suite:
+        suite = unittest.TestSuite()
+        suite.addTest(CommunicationTestCase('test_breakpoints'))
+        return suite
+    else:
+        return None        
+
 
 if __name__ == '__main__':
     getopts()
-    mysocket.connect_to(PORT)
-    print("Please, manually check that the simulator has also finished after running all the tests.")
-    unittest.main()
-    mysocket.close_connection()
-    mysocket.close_socket()
+    if suite():
+        unittest.TextTestRunner().run(suite())
+    else:
+        unittest.main()        
