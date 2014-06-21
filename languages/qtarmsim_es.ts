e1043b56dd2e9e827ad7e78e52953eaf5d8f8e81
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2014-06-21 16:27:22 +0200

    Move from bzr to git

---
+++ languages/qtarmsim_es.ts
@@ -0,0 +1,442 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Help</name>
+    <message>
+        <location filename="ui/help.py" line="35"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QtArmSimMainWindow</name>
+    <message>
+        <location filename="qtarmsim.py" line="105"/>
+        <source>Abrir Archivo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="105"/>
+        <source>Archivos de ensamblador (*.s *.asm);;Todos los archivos (*)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="105"/>
+        <source>Archivos de ensamblador (*.s *.asm)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="108"/>
+        <source>Simulador GlSpim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="108"/>
+        <source>Cargando %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="126"/>
+        <source>Save File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="126"/>
+        <source>Archivos de salida (*.out)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="140"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="140"/>
+        <source>No se puede escribir %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="146"/>
+        <source>Regitros:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="148"/>
+        <source>
+Segmento de Datos:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="150"/>
+        <source>
+Segmento de Texto:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="152"/>
+        <source>
+
+Mensajes:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="154"/>
+        <source>
+
+Consola:
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="158"/>
+        <source>File saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="240"/>
+        <source>Ejecutando instrucción</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="261"/>
+        <source>Detener ejecución</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="261"/>
+        <source>Quieres detener la ejecución del programa?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="288"/>
+        <source>Limpiando consola</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="314"/>
+        <source>Limpiando registros</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="320"/>
+        <source>Recargando el archivo actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="327"/>
+        <source>Restaurando contenidos de registros y memoria</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="365"/>
+        <source>About Qt ArmSim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="373"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="365"/>
+        <source>Based on the code for a graphical frontend for Spim
+developed by Gloria Edo Piñana on 2008.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="373"/>
+        <source>About ArmSim</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="517"/>
+        <source>Abrir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="391"/>
+        <source>Salir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="394"/>
+        <source>Guardar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="397"/>
+        <source>Imprimir</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="400"/>
+        <source>Opciones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="525"/>
+        <source>Ejecutar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="409"/>
+        <source>Ejecución multipasos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="533"/>
+        <source>Puntos de ruptura</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="415"/>
+        <source>Fijar valor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="418"/>
+        <source>Ayuda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="421"/>
+        <source>Limpiar registros</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="424"/>
+        <source>Reinicializar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="427"/>
+        <source>Recargar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="430"/>
+        <source>Parar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="433"/>
+        <source>Limpiar Consola</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="437"/>
+        <source>B&amp;arra de estado</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="441"/>
+        <source>Oculta o hace visible la barra de estado</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="442"/>
+        <source>Barra de estado</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="444"/>
+        <source>R&amp;estaurar disposición por defecto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="446"/>
+        <source>Recupera la disposición por defecto de la ventana principal del programa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="447"/>
+        <source>Recuperar disposición por defecto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="464"/>
+        <source>Barra de herramientas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="465"/>
+        <source>Oculta o hace visible la barra de tareas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="475"/>
+        <source>Panel de registros</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="476"/>
+        <source>Oculta o hace visible el panel de registros</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="480"/>
+        <source>Panel de segmento de texto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="481"/>
+        <source>Oculta o hace visible el panel de segmento de texto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="486"/>
+        <source>Panel de segmento de datos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="487"/>
+        <source>Oculta o hace visible el panel de segmento de datos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="493"/>
+        <source>Panel de mensajes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="494"/>
+        <source>Oculta o hace visible el panel de mensajes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="515"/>
+        <source>Permite especificar el nombre del fichero que debe ser cargado y ensamblado en memoria</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="516"/>
+        <source>Abrir un archivo existente</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="519"/>
+        <source>Permite guardar en un archivo .out el estado actual de los segmentos de texto y datos, de los registros, los mensajes y la consola</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="520"/>
+        <source>Capturar el estado actual en un archivo .out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="521"/>
+        <source>Capturar estado actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="523"/>
+        <source>Sirve para ejecutar el programa cargado en memoria. Antes de comenzar la ejecución un diálogo permite especificar la dirección de comienzo de la ejecución</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="524"/>
+        <source>Ejecutar el programa actual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="527"/>
+        <source>Permite detener la ejecución en curso</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="528"/>
+        <source>Detener la ejecución</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="529"/>
+        <source>Detener</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="531"/>
+        <source>Sirve para introducir o borrar puntos de ruptura en la ejecución del programa. Un cuadro de diálogo permite añadir las direcciones de memoria en las que se desea detener la ejecución</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="532"/>
+        <source>Añadir punto de ruptura</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="535"/>
+        <source>Activa la ayuda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="536"/>
+        <source>Activar la ayuda</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="537"/>
+        <source>¿Qué es esto?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="544"/>
+        <source>Listo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="552"/>
+        <source>Registros</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="558"/>
+        <source>Courier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="569"/>
+        <source>Panel de visualización de registros, muestra los valores de los registros del procesador MIPS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="576"/>
+        <source>Segmento de texto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="588"/>
+        <source>Panel de visualización de código, muestra las instrucciones del programa de usuario y del núcleo del sistema que se carga automáticamente cuando se inicia XSPIM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="595"/>
+        <source>Segmento de datos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="607"/>
+        <source>Panel de visualización de datos, muestra el contenido de la memoria</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="qtarmsim.py" line="618"/>
+        <source>Mensajes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="qtarmsim.py" line="629"/>
+        <source>Panel de visualización de mensajes, lo utiliza el simulador para informar de qué está haciendo y avisar de los errores que ocurran durante el ensamblado o ejecución de un programa</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
