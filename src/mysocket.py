c36dd0971cf10c88da81cde1c8790ed235f5d935
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2014-07-01 17:40:28 +0200

    Corrected bug on mysocket.py

--- src/mysocket.py
+++ src/mysocket.py
@@ -24,24 +24,28 @@ import sys
 class MySocket:
     
     MSGLEN = 1024
-    NL = b'\n'
+    NL = '\n'
     ENCODING = 'utf8'
 
     def __init__(self, verbose=False):
-        "Creates and initializes the socket"
+        """
+        Initializes the socket
+        """
         self.verbose = verbose
-        if self.verbose:
-            print('Creating the socket')
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Set conn to None
         self.conn = None
-        # Set pending_lines to empty
         self.pending_lines = []
-        # Asign CTRL+C signal
         signal.signal(signal.SIGINT, self.exit_signal_handler)
 
     def server_bind(self, port):
-        """Binds the socket to a given port and starts listening. Should be used by the server application. Returns -1 if something went wrong."""
+        """
+        Binds the socket to a given port and starts listening on that port.
+        
+        This method should be used by a server application. Returns -1 if
+        something goes wrong.
+        """
+        if self.verbose:
+            print('Creating the socket')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             if self.verbose:
                 print('Binding to port {}'.format(port))
@@ -51,13 +55,18 @@ class MySocket:
             sys.exit(-1)
         if self.verbose:
             print('Socket bind complete')
+        # Only one listener
         self.sock.listen(1)
         if self.verbose:
             print('Socket now listening')
         return 0
 
     def server_accept_connection(self):
-        "Waits till a connection is done. This is a blocking call. Should be used by a server application."
+        """
+        Waits till a connection is done.
+        
+        This is a blocking call. Should be used by a server application.
+        """
         if self.verbose:
             print('Waiting for a connection')
         self.conn, addr = self.sock.accept()
@@ -65,7 +74,14 @@ class MySocket:
             print('Connected with ' + addr[0] + ':' + str(addr[1]))
         
     def connect_to(self, port, server="localhost"):
-        "Used by a client to establish a connection to the specified port."
+        """
+        Establishes a connection to the given server at the given port.
+        
+        This method should be used by a client application.
+        """
+        if self.verbose:
+            print('Creating the socket')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((server, port))
         self.conn = self.sock
         
@@ -89,17 +105,22 @@ class MySocket:
                 
 
     def receive_line(self):
+        """
+        Returns a line from the line received queue or gets a new one.
+        
+        @return: a line.
+        """
         if len(self.pending_lines):
             return self.pending_lines.pop(0)
         data = self.conn.recv(self.MSGLEN)
         msg = data.decode(self.ENCODING)
-        while (len(data) == self.MSGLEN and data[-1] != self.NL):
+        while (len(data) == self.MSGLEN and msg[-1] != self.NL) or msg.strip() == '':
             data = self.conn.recv(self.MSGLEN)
             if len(data) == 0: # Connection closed
-                #@warning: even in this case we should continue, as there could be previous read lines
+                # @warning: even in this case we should continue, as there could be previous read lines
                 break
             msg += data.decode(self.ENCODING)
-        lines = msg.strip().split('\r\n')
+        lines = [l.strip() for l in msg.strip().replace('\r\n', '\n').split('\n') if l.strip() != ""]
         if len(lines)>1:
             self.pending_lines = lines[1:]
         return lines[0]
@@ -108,32 +129,49 @@ class MySocket:
         """
         Receives lines until a line with the EOF word is received.
         
-        @return: An array with the received lines or an empty array if a timeout occurs
+        @return: An array with the received lines or an empty array if a timeout occurs.
         """    
         lines = []
-        line = self.receive_line()
+        line = ''
         while line != 'EOF':
+            try:
+                line = self.receive_line()
+            except socket.timeout:
+                print("TimeOut Error")
+                print("\n".join(lines))
+                raise
             lines.append(line)
-            line = self.receive_line()
         if line == 'EOF': 
-            return lines
+            return lines[:-1]
         else: # timeout occurred
             return []
     
     def send_line(self, msg):
+        """
+        Sends a line through the open connection.
+        """
         self.conn.sendall(bytes(msg, self.ENCODING)+b'\r\n')
         
     def close_connection(self):
+        """
+        Closes the current connection.
+        """
         if self.conn:
             self.conn.close()
             if self.verbose:
                 print('Connection closed')
 
     def close_socket(self):
+        """
+        Closes the current socket.
+        """
         self.sock.close()
         if self.verbose:
             print('Socket closed')
 
             
     def exit_signal_handler(self, signal, frame):
+        """
+        Handler used to closes the socket when an exit signal is received. See __init__().
+        """
         self.close_socket()
