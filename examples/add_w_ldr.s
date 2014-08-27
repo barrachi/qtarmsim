991e5f9c1bf3a3c38a34c7f8e06fa925854e861b
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2014-08-27 18:04:10 +0200

    Added a new example

---
+++ examples/add_w_ldr.s
@@ -0,0 +1,19 @@
+@
+@ Qt ARMSim example (http://lorca.act.uji.es/projects/qtarmism/
+@
+	.data
+a:	.word 0x20
+b:	.word 0x30
+res:	.space 4
+
+	.text
+
+main:	ldr r0, =a	@ r0 <- a
+	ldr r1, [r0]	@ r1 <- [a]
+	ldr r0, =b	@ r0 <- b
+	ldr r2, [r0]	@ r2 <- [b]
+	add r3, r1, r2	@ r3 <- r1 + r2
+	ldr r0, =res	@ r0 <- res
+	str r3, [r0]	@ [r0] <- r1 + r2
+
+end:	wfi
