649dc4a409aa55af09cc500c0db633ed8d460500
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2015-11-28 20:39:19 +0100

    Cleanup

--- qtarmsim/lang/qtarmsim_es.ts
+++ qtarmsim/lang/qtarmsim_es.ts
@@ -11,214 +11,214 @@
 <context>
     <name>QtARMSimMainWindow</name>
     <message>
-        <location filename="mainwindow.py" line="151"/>
+        <location filename="mainwindow.py" line="158"/>
         <source>Ready</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="247"/>
+        <location filename="mainwindow.py" line="255"/>
         <source> [modified] - </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="379"/>
+        <location filename="mainwindow.py" line="387"/>
         <source>&lt;b&gt;{} assembled.&lt;/b&gt;
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="394"/>
+        <location filename="mainwindow.py" line="402"/>
         <source>File not found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="394"/>
+        <location filename="mainwindow.py" line="402"/>
         <source>Assemble File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="406"/>
+        <location filename="mainwindow.py" line="414"/>
         <source>&lt;b&gt;Assembly errors:&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="410"/>
+        <location filename="mainwindow.py" line="418"/>
         <source>(Something bad has happened. That&apos;s all I know.)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="414"/>
+        <location filename="mainwindow.py" line="422"/>
         <source>An error has occurred when assembling the source code.
 Please, see the Messages panel for more details.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="414"/>
+        <location filename="mainwindow.py" line="422"/>
         <source>Assembly Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="427"/>
+        <location filename="mainwindow.py" line="435"/>
         <source>Set breakpoint error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="437"/>
+        <location filename="mainwindow.py" line="445"/>
         <source>Clear breakpoint error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="445"/>
+        <location filename="mainwindow.py" line="453"/>
         <source>Set Register Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="452"/>
+        <location filename="mainwindow.py" line="460"/>
         <source>Set Memory Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="553"/>
+        <location filename="mainwindow.py" line="561"/>
         <source>untitled.s</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="528"/>
+        <location filename="mainwindow.py" line="536"/>
         <source>Open File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="566"/>
+        <location filename="mainwindow.py" line="574"/>
         <source>ARM assembler files (*.s);;ARM C files (*.c)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="537"/>
+        <location filename="mainwindow.py" line="545"/>
         <source>Will try next with &apos;{}&apos; encoding.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="540"/>
+        <location filename="mainwindow.py" line="548"/>
         <source>No more supported encodings.
 Please, manually convert the file to &apos;utf-8&apos; and load it again.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="540"/>
+        <location filename="mainwindow.py" line="548"/>
         <source>Couldn&apos;t read the file using the &apos;{}&apos; encoding.
 {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="541"/>
+        <location filename="mainwindow.py" line="549"/>
         <source>Error reading &apos;{}&apos;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="564"/>
+        <location filename="mainwindow.py" line="572"/>
         <source>Save File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="595"/>
+        <location filename="mainwindow.py" line="603"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="596"/>
+        <location filename="mainwindow.py" line="604"/>
         <source>Could not write to file &apos;{0}&apos;:
 {1}.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="601"/>
+        <location filename="mainwindow.py" line="609"/>
         <source>File saved</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="751"/>
+        <location filename="mainwindow.py" line="759"/>
         <source>Ejecutando instrucción</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="772"/>
+        <location filename="mainwindow.py" line="780"/>
         <source>Detener ejecución</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="773"/>
+        <location filename="mainwindow.py" line="781"/>
         <source>Quieres detener la ejecución del programa?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="780"/>
+        <location filename="mainwindow.py" line="788"/>
         <source>Limpiando consola</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="789"/>
+        <location filename="mainwindow.py" line="797"/>
         <source>Limpiando registros</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="795"/>
+        <location filename="mainwindow.py" line="803"/>
         <source>Recargando el archivo actual</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="802"/>
+        <location filename="mainwindow.py" line="810"/>
         <source>Restaurando contenidos de registros y memoria</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="846"/>
+        <location filename="mainwindow.py" line="854"/>
         <source>version</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="849"/>
+        <location filename="mainwindow.py" line="857"/>
         <source>Based on the graphical frontend for Spim developed on 2008 by Gloria Edo Piñana.&lt;br&gt;&lt;/br&gt;
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="851"/>
+        <location filename="mainwindow.py" line="859"/>
         <source>Developed at the Jaume I University, Castellón, Spain.&lt;br&gt;&lt;/br&gt;
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="853"/>
+        <location filename="mainwindow.py" line="861"/>
         <source>Version</source>
         <translation type="unfinished"></translation>
     </message>
     <message encoding="UTF-8">
-        <location filename="mainwindow.py" line="855"/>
+        <location filename="mainwindow.py" line="863"/>
         <source>&lt;p&gt;Based on the graphical frontend for Spim&lt;br/&gt;developed on 2008 by Gloria Edo Piñana.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="859"/>
+        <location filename="mainwindow.py" line="867"/>
         <source>Running on </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="867"/>
+        <location filename="mainwindow.py" line="875"/>
         <source>About QtARMSim</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="874"/>
+        <location filename="mainwindow.py" line="882"/>
         <source>About ARMSim</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="932"/>
+        <location filename="mainwindow.py" line="941"/>
         <source>ARMSim command empty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="934"/>
+        <location filename="mainwindow.py" line="943"/>
         <source>ARMSim command is empty.
 
 Please go to &apos;Edit, Preferences...&apos; and set it.
@@ -226,12 +226,12 @@ Please go to &apos;Edit, Preferences...&apos; and set it.
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="937"/>
+        <location filename="mainwindow.py" line="946"/>
         <source>ARM gcc not found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="939"/>
+        <location filename="mainwindow.py" line="948"/>
         <source>ARM gcc command not found.
 
 Please go to &apos;Edit, Preferences...&apos; and set it.
@@ -239,24 +239,24 @@ Please go to &apos;Edit, Preferences...&apos; and set it.
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="942"/>
+        <location filename="mainwindow.py" line="951"/>
         <source>Connecting to ARMSim...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="954"/>
+        <location filename="mainwindow.py" line="963"/>
         <source>Connection to ARMSim failed
 
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="959"/>
+        <location filename="mainwindow.py" line="968"/>
         <source>Connected to ARMSim at port {}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.py" line="966"/>
+        <location filename="mainwindow.py" line="975"/>
         <source>ARMSim set setting failed</source>
         <translation type="unfinished"></translation>
     </message>
