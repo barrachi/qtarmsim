cf0b25cded839a95c8ab218381c698c2b77b77a5
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2018-07-24 10:54:48 +0200

    Added memory stack example

---
+++ qtarmsim/examples/02_Memory/02_stack.s
@@ -0,0 +1,18 @@
+@
+@ This program:
+@  + initialices the registers r1 to r3,
+@  + pushes the registers r1 to r3 in the stack,
+@  + modifies the registers r1 to r3,
+@  + pops their previous values from the stack.
+@
+
+	.text
+	mov r1, #0x01
+	mov r2, #0x02
+	mov r3, #0x03
+	push {r1-r3}
+	mov r1, #0x11
+	mov r2, #0x22
+	mov r3, #0x33
+	pop {r1-r3}
+	wfi
