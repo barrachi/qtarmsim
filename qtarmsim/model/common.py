7af0f603564da198766392070898ad7812045e28
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2018-07-26 11:53:59 +0200

    Changed the way a monospaced font is selected

--- qtarmsim/model/common.py
+++ qtarmsim/model/common.py
@@ -105,9 +105,13 @@ class InputToHex(QtCore.QObject):
 
 
 def getMonoSpacedFont():
-    font = QtGui.QFont("fake font name")  # @warning: fake name needed to setStyleHint work
-    font.setStyleHint(QtGui.QFont.TypeWriter)
+    font = QtGui.QFontDatabase.systemFont(QtGui.QFontDatabase.FixedFont)
+    # If the previous line does not work...
     if not QtGui.QFontInfo(font).fixedPitch():
+        font = QtGui.QFont("Monospace")
         font.setStyleHint(QtGui.QFont.Monospace)
+        # If we are not there yet...
+        if not QtGui.QFontInfo(font).fixedPitch():
+            font.setStyleHint(QtGui.QFont.TypeWriter)
     font.setPointSize(QtGui.QFont().pointSize())  # Using the system default font point size
     return font
