22e2cfd4d7744a1593b6aa3f3640a5bb1b26a6bf
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2026-02-04 16:39:57 +0000

    Updated progress bar example to the new PySide6 API

--- misc/examples/progress_bar.py
+++ misc/examples/progress_bar.py
@@ -3,51 +3,53 @@
 
 import sys
 import time
-from PySide import QtGui, QtCore
+from PySide6 import QtGui, QtCore, QtWidgets
 
 
-class MyCustomWidget(QtGui.QWidget):
+class MyCustomWidget(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(MyCustomWidget, self).__init__(parent)
-        layout = QtGui.QVBoxLayout(self)
+        layout = QtWidgets.QVBoxLayout(self)
 
         # Create a progress bar and a button and add them to the main layout
-        self.progressBar = QtGui.QProgressBar(self)
-        self.progressBar.setRange(0,1)
+        self.progressBar = QtWidgets.QProgressBar(self)
+        self.progressBar.setRange(0, 1)
         layout.addWidget(self.progressBar)
-        button = QtGui.QPushButton("Start", self)
-        layout.addWidget(button)      
+        button = QtWidgets.QPushButton("Start", self)
+        layout.addWidget(button)
 
         button.clicked.connect(self.onStart)
 
         self.myLongTask = TaskThread()
         self.myLongTask.taskFinished.connect(self.onFinished)
 
-    def onStart(self): 
-        self.progressBar.setRange(0,0)
+    def onStart(self):
+        self.progressBar.setRange(0, 0)
         self.myLongTask.start()
 
     def onFinished(self):
         # Stop the pulsation
-        self.progressBar.setRange(0,1)
+        self.progressBar.setRange(0, 1)
 
 
 class TaskThread(QtCore.QThread):
-    taskFinished = QtCore.pyqtSignal()
+    taskFinished = QtCore.Signal()
+
     def run(self):
         time.sleep(3)
         self.taskFinished.emit()
-        
+
+
 def main():
     # Create the application
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     # Create the main window and show it
     main_window = MyCustomWidget()
     main_window.show()
     # Enter the main loop of the application
     sys.exit(app.exec_())
-    
-        
+
+
 if __name__ == "__main__":
     main()
