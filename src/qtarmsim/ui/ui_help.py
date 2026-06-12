0fc5e8104f8acfa32e07f487d73f422da68ae719
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2026-06-12 18:01:01 +0200

    The resources should be on git to allow the executables access to them

---
+++ src/qtarmsim/ui/ui_help.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'help.ui'
+##
+## Created by: Qt User Interface Compiler version 6.11.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QSizePolicy, QWidget)
+
+class Ui_Help(object):
+    def setupUi(self, Help):
+        if not Help.objectName():
+            Help.setObjectName(u"Help")
+        Help.resize(562, 524)
+
+        self.retranslateUi(Help)
+
+        QMetaObject.connectSlotsByName(Help)
+    # setupUi
+
+    def retranslateUi(self, Help):
+        Help.setWindowTitle(QCoreApplication.translate("Help", u"Help", None))
+    # retranslateUi
+
