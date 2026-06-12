d5d180e95c62a497999a1ff32501f76fe7a7876c
Sergio Barrachina Mir <sergio.barrachina@gmail.com>
2026-06-12 17:44:51 +0200

    Release 2.0.2

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
         #     adding pyside6 to nativeBuildInputs puts those tools on PATH.
         qtarmsim = pkgs.python3Packages.buildPythonApplication {
           pname   = "qtarmsim";
-          version = "2.0.1";
+          version = "2.0.2";
           src     = self;
           pyproject = true;
 
@@ -157,7 +157,7 @@
             "--prefix" "PATH" ":" "${pkgs.gcc-arm-embedded}/bin"
           ];
 
-          env.SETUPTOOLS_SCM_PRETEND_VERSION = "2.0.1";
+          env.SETUPTOOLS_SCM_PRETEND_VERSION = "2.0.2";
 
           meta = with pkgs.lib; {
             description = "Easy to use graphical ARM simulator";
